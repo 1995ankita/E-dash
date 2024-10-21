4a2c93087f2f862a72f85d7acf22aaad78e9a52a
1995ankita <ankita.hirpara@theknowledgeacademy.com>
2024-10-21 18:23:41 +0530

    front-end sign up

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
+import Nav from './components/Nav';
+import Footer from './components/Footer';
+import SignUp from './components/SignUp';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+      <Nav></Nav>
+      <Routes>
+      <Route path="/" element={<h1>Product listing Component</h1>}></Route>
+      <Route path="/add" element={<h1>Add Component</h1>}></Route>
+      <Route path="/update" element={<h1>Upadte  Component</h1>}></Route>
+      <Route path="/logout" element={<h1>logout Component</h1>}></Route>
+      <Route path="/profile" element={<h1>profile Component</h1>}></Route>
+      <Route path="/signup" element={<SignUp></SignUp>}></Route>
+      </Routes>
+     </BrowserRouter>
+      <Footer></Footer>
     </div>
   );
 }
